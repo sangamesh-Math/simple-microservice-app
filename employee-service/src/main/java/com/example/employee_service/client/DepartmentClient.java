2b8d62aff1431b2794de2e506fb6132915f226e9
sangamesh <sangamesh.math@ascendion.com>
2025-11-19 20:29:09 +0530

    test code cleanup commit

--- employee-service/src/main/java/com/example/employee_service/client/DepartmentClient.java
+++ employee-service/src/main/java/com/example/employee_service/client/DepartmentClient.java
@@ -1,7 +1,6 @@
 package com.example.employee_service.client;
 
 import com.example.employee_service.dto.DepartmentDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,15 +8,11 @@ import org.springframework.web.client.RestTemplate;
 public class DepartmentClient {
 
     private final RestTemplate restTemplate;
-//    @Value("${department.service.url}")
-//    private String baseUrl;
-
     public DepartmentClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public DepartmentDto getDepartment(Long id) {
-//        String url = baseUrl + "/api/departments/" + id;
         String url = "http://DEPARTMENT-SERVICE/api/departments/" + id;
         System.out.println("Printing url: " + url);
         return restTemplate.getForObject(url, DepartmentDto.class);
